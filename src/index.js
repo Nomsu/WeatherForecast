cb968730257cd0ad8e100309c92023b09aa38b53
Faith Olaniran <fmark1455@gmail.com>
2024-01-25 17:18:18 +0100

    js and index updated

--- src/index.js
+++ src/index.js
@@ -52,23 +52,38 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let forecast = document.querySelector("#forecast");
+    
+
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+    let forecastHtml ="";
+
+    days.forEach(function (day) {
+        forecastHtml =  
+        forecastHtml +
+        `
+        <div class="weather-forecast">
+            <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-icon">🌤️</div>
+                <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                    <strong>15º</strong>
+                </div>
+                <div class="weather-forecast-temperature">9º</div>
+            </div>
+        </div>
+        `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Ibadan");
 
-let forecast = document.querySelector("#forecast");
-
-forecast.innerHTML = `
-    <div class="weather-forecast">
-        <div class="weather-forecast-day">
-            <div class="weather-forecast-date">Tue</div>
-            <div class="weather-forecast-icon">🌤️</div>
-            <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature">
-                <strong>15º</strong>
-              </div>
-            <div class="weather-forecast-temperature">9º</div>
-        </div>
-    </div>
-`;
+displayForecast();
